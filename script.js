b076f57145cd25971ba29e2fb0ab4400d646f806
Jessie Coney <jessie.coney@eagles.ewu.edu>
2018-11-01 10:18:41 -0700

    cloned and edited

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$.simpleWeather({
+    location: 96785,
+    success: function(weather) {
+      console.log(weather.image);
+      console.log(weather.city);
+      console.log(weather.temp);
+      console.log(weather.updated);
+
+      $('.image img').attr('src', weather.image);
+      $('.city').text(weather.city);
+      $('.temp').text(weather.temp);
+      $('.time').text(weather.updated);
+
+    }, 
+    error: function(error) {
+      console.log('Go outside!');
+    }
+ });
+
