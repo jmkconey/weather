317fab37015f78aed0165b3d0302fbee2d30f334
Jessie Coney <jessie.coney@eagles.ewu.edu>
2018-11-06 11:21:22 -0800

    Added in conditionals for no geo vs with geo

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,56 @@
+// Geolocation Check
+if ( 'geolocation' in navigator ) {
+  
+  $('.geo').show();
+  $('.no-geo').hide();
+  
+} else {
+  
+  $('.geo').hide();
+  $('.no-geo').show();
+  
+}
 
-$.simpleWeather({
-    location: 96785,
+// Get Weather
+$('button').click( function(){
+ 
+     navigator.geolocation.getCurrentPosition(function(position) {
+      
+      // Check lat/long coordinates
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      
+      console.log(lat, long);
+      
+      // Get Weather
+      getWeather( lat + ',' + long );
+       
+     });
+});
+
+
+// Define Get Weather
+var getWeather = function( location ) {
+  
+   console.log(location);
+  
+   $.simpleWeather({
+    location: location,
+    unit: 'f',
     success: function(weather) {
-      console.log(weather.image);
-      console.log(weather.city);
-      console.log(weather.temp);
-      console.log(weather.updated);
+      
+      // Entire weather object
+      // console.log(weather);
+      
+      // Display Data
+      $('h1').text(weather.city);
+      $('h2').text(weather.temp);
 
-      $('.image img').attr('src', weather.image);
-      $('.city').text(weather.city);
-      $('.temp').text(weather.temp);
-      $('.time').text(weather.updated);
-
-    }, 
+    },
     error: function(error) {
-      console.log('Go outside!');
+      // Show if weather cannot be retreived
+      console.log('Look Outside.');
     }
- });
-
+  
+  });
+};
