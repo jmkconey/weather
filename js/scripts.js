d1cecd509c3cb92cfaf9df0822f6de31f3197c43
Jessie Coney <jessie.coney@eagles.ewu.edu>
2018-11-06 11:50:24 -0800

    edited html

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
-
+// Primary
 $.simpleWeather({
-  location: 96785,
+  location: 98105,
   success: function(weather) {
-    console.log(weather.image);
-    console.log(weather.city);
-    console.log(weather.temp);
-    console.log(weather.updated);
-
-    $('.image img').attr('src', weather.image);
-    $('.city').text(weather.city);
-    $('.temp').text(weather.temp);
-    $('.time').text(weather.updated);
+     
+    $('#primary .image img').attr( 'src', weather.image  );
+    $('#primary .city').text(weather.city);
+    $('#primary .temp').text(weather.temp);
+    $('#primary .time').text(weather.updated);
 
   }, 
   error: function(error) {
@@ -18,61 +14,77 @@ $.simpleWeather({
   }
 });
 
+//  Secondary
+$.simpleWeather({
+location: 'Rainier, WA',
+success: function(weather) {
+   
+  $('#secondary .image img').attr( 'src', weather.image  );
+  $('#secondary .city').text(weather.city);
+  $('#secondary .temp').text(weather.temp);
+  $('#secondary .time').text(weather.updated);
 
+}, 
+error: function(error) {
+  console.log('Go outside!');
+}
+});
 
+// Geolocation
 // Geolocation Check
 if ( 'geolocation' in navigator ) {
-  
-  $('.geo').show();
-  $('.no-geo').hide();
-  
+
+$('#geolocation').show();
+
 } else {
-  
-  $('.geo').hide();
-  $('.no-geo').show();
-  
+
+$('#geolocation *').hide();
+$('#geolocation').html('<p>Not Available</p>');
+
 }
 
 // Get Weather
 $('button').click( function(){
+
+navigator.geolocation.getCurrentPosition(function(position) {
  
-     navigator.geolocation.getCurrentPosition(function(position) {
-      
-      // Check lat/long coordinates
-      var lat = position.coords.latitude;
-      var long = position.coords.longitude;
-      
-      console.log(lat, long);
-      
-      // Get Weather
-      getWeather( lat + ',' + long );
-       
-     });
+ // Check lat/long coordinates
+ var lat = position.coords.latitude;
+ var long = position.coords.longitude;
+ 
+ console.log(lat, long);
+ 
+ // Get Weather
+ getWeather( lat + ',' + long );
+  
+});
 });
 
 
 // Define Get Weather
 var getWeather = function( location ) {
-  
-   console.log(location);
-  
-   $.simpleWeather({
-    location: location,
-    unit: 'f',
-    success: function(weather) {
-      
-      // Entire weather object
-      // console.log(weather);
-      
-      // Display Data
-      $('h1').text(weather.city);
-      $('h2').text(weather.temp);
 
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-      console.log('Look Outside.');
-    }
-  
-  });
+console.log(location);
+
+$.simpleWeather({
+location: location,
+unit: 'f',
+success: function(weather) {
+ 
+ // Entire weather object
+ // console.log(weather);
+ 
+ // Display Data
+ $('#geolocation .image img').attr( 'src', weather.image  );
+ $('#geolocation .city').text(weather.city);
+ $('#geolocation .temp').text(weather.temp);
+ $('#geolocation .time').text(weather.updated);
+
+},
+error: function(error) {
+ // Show if weather cannot be retreived
+ console.log('Look Outside.');
+}
+
+});
 };
